f6ced05b51e1a8477fcbab1bdf7f3c4b21c58479
Florid6666 <pratyushkumarsethi37@gmail.com>
2024-12-25 19:46:18 +0530

    First commit

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import NotFound from "./containers/NotFound";
 import Home from "./containers/Home";
 
 export default function Routes() {
@@ -8,6 +9,11 @@ export default function Routes() {
       <Route exact path="/">
         <Home />
       </Route>
+      {/* Finally, catch all unmatched routes */}
+      <Route>
+        <NotFound />
+      </Route>
+      
     </Switch>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "prats-bucket",
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://qxxmaebpg2.execute-api.us-east-1.amazonaws.com/prod",
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_tuMLfJ0Ca",
+      APP_CLIENT_ID: "5h3tvgkue7n3lcob1n8m62eo85",
+      IDENTITY_POOL_ID: "us-east-1:c40d6d07-dde4-4b5f-8331-5249568eae31",
+    },
+};
+
+export default config;
