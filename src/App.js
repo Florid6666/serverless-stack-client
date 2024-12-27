a94420d3a5599b0118cf6bc661baae0277cabe39
Florid6666 <pratyushkumarsethi37@gmail.com>
2024-12-27 15:22:08 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -43,30 +43,35 @@ function App() {
             </Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle />
-           <Navbar.Collapse className="justify-content-end">
-             <Nav activeKey={window.location.pathname}>
-               {isAuthenticated ? (
-                 <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
-               ) : (
-                 <>
-                   <LinkContainer to="/signup">
-                     <Nav.Link>Signup</Nav.Link>
-                   </LinkContainer>
-                   <LinkContainer to="/login">
-                     <Nav.Link>Login</Nav.Link>
-                   </LinkContainer>
-                 </>
-               )}
-             </Nav>
-           </Navbar.Collapse>
-         </Navbar>
-         <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
-           <Routes />
-         </AppContext.Provider>
-       </div>
-     )
+          <Navbar.Collapse className="justify-content-end">
+            <Nav activeKey={window.location.pathname}>
+              {isAuthenticated ? (
+                <>
+                  <LinkContainer to="/settings">
+                    <Nav.Link>Settings</Nav.Link>
+                  </LinkContainer>
+                  <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+                </>
+              ) : (
+                <>
+                  <LinkContainer to="/signup">
+                    <Nav.Link>Signup</Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to="/login">
+                    <Nav.Link>Login</Nav.Link>
+                  </LinkContainer>
+                </>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+          <Routes />
+        </AppContext.Provider>
+      </div>
+    )
   );
-
+    
 }
 
 export default App;
