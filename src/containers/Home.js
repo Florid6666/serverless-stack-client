90d6158b2595cbee301aa30a5328c354d4ef1688
Florid6666 <pratyushkumarsethi37@gmail.com>
2025-01-24 00:53:31 +0530

    First commit

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -63,7 +63,10 @@ export default function Home() {
       <div className="lander">
         <h1>Scratch</h1>
         <p className="text-muted">A simple note taking app</p>
-        <div className="notebook"></div>
+        <div className="notebook-container">
+                <div className="notebook"></div>
+                <div className="pen"></div>
+          </div>
       </div>
     );
   }
